6950cf1aa701a8e5bf7d47149722c1fc304e9859
deepikaappana <amadhudeepikar879@gmail.com>
2026-03-09 19:26:19 +0530

    leftsidebar

---
+++ src/Components/rightsidebar/rightsidebar.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rightsidebar',
+  imports: [],
+  templateUrl: './rightsidebar.html',
+  styleUrl: './rightsidebar.scss',
+})
+export class Rightsidebar {
+
+}
