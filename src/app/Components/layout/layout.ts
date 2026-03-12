997cf394120c9de185467195952076ad337b67cb
deepikaappana <amadhudeepikar879@gmail.com>
2026-03-12 17:35:27 +0530

    explorepage

--- src/app/Components/layout/layout.ts
+++ src/app/Components/layout/layout.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { Header } from '../header/header';
 import { Footer } from '../footer/footer';
-import { Landing } from '../landing/landing';
 import { Leftsidebar } from './Components/leftsidebar/leftsidebar';
+import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [Header,Footer,Landing,Leftsidebar],
+  imports: [Header,Footer,Leftsidebar,RouterOutlet],
   templateUrl: './layout.html',
   styleUrl: './layout.scss',
 })
