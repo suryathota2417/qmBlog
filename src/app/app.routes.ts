2c850f0085a93334ff206be7f6ef31fb2ec3e0bf
deepikaappana <amadhudeepikar879@gmail.com>
2026-03-18 17:01:32 +0530

    latest-createpost-changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { Postdetailspage } from './Components/Posts-Pages/postdetailspage/postde
 import { Signin } from './Components/Accounts/Authentications/signin/signin';
 import { Signup } from './Components/Accounts/Authentications/signup/signup';
 import { Profile } from './Components/Accounts/ProfileData/profile/profile';
+import { Createpostpage } from './Components/Posts-Pages/createpostpage/createpostpage';
 
 export const routes: Routes = [
+  {path: '', component: Explorepage},
   {path: 'signin', component: Signin},
   {path: 'signup', component: Signup},
-  { path: '', component: Explorepage },
+  { path: 'create-post', component: Createpostpage },
   {
     path: 'post/:id',
     component: Postdetailspage,
